a7b1604c50e1d0ad3b44a2cf2c3ac8ab282b807f
Alekomusic12 <117124383+Alekomusic12@users.noreply.github.com>
2023-03-03 19:02:20 -0500

    v11

--- controller/fileController.js
+++ controller/fileController.js
@@ -12,6 +12,7 @@ exports.createProduct = async(req, res) =>  {
             msg: 'la cedula ya existe, contacte a su administrador'
         })
 
+
         const dbFile = new File({
             modelo: modelo,
             fabricante: fabricante,
@@ -38,3 +39,57 @@ exports.createProduct = async(req, res) =>  {
     }
 }
 
+exports.obtenerProduct = async(req, res) => {
+    try {
+        const{id} = req.params
+        const file = await File.findById(id)
+        if(!file) return res.status(404).json({
+            ok: false,
+            msg: 'el archivo solicitado no existe'
+        })
+         return res.status(200).json({
+            ok:true,
+            msg: 'la consulta fue exitosa',
+            file
+
+         })
+
+    } catch (error) {
+        res.status(404).send('Algo pasa a la hora de buscar un producto especifico')
+        
+    } }
+
+    exports.deleteproduct = async(req, res) => {
+        try {
+            
+        } catch (error) {
+            res.status(403).send('algo pasa y no elmina el producto')
+            
+        }
+    } 
+
+    exports.modifyproducto = async(req, res) => {
+        try {
+            
+        } catch (error) {
+            res.status(500).send('algo pasa y no modifica')
+        }
+    }
+
+
+    exports.obtenerProductos = async(req, res) => {
+        try {
+            const file = await File.find()
+            return res.status(200).json({
+                ok:true,
+                file
+            })
+        } catch (error) {
+            return res.status(500).json({
+                ok:false,
+                msg: 'ups el archivo esta en veremos, pero si se pude', 
+            })
+            
+        }
+    } 
+    
--- routes/file.routes.js
+++ routes/file.routes.js
@@ -4,5 +4,9 @@ const controladorPeoduct = require('../controller/fileController')
 
 //ruta contacto
 router.post('/create-product',controladorPeoduct.createProduct)
+router.get('/obtener-product/:id',controladorPeoduct.obtenerProduct)
+router.get('/obtener-product',controladorPeoduct.obtenerProductos)
+// router.delete('/delete-product',controlador.elmininarProduct)
+// router.put('/modify-producto',controlado.modificarProduct)
 
 module.exports = router
